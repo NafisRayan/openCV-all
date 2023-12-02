3fa6b723a6f10ed8a38f918a465b1059452a1062
Nafis Rayan <90589446+NafisRayan@users.noreply.github.com>
2023-12-03 02:51:47 +0600

    Update yolo-pose.py

--- YOLOv8/yolo-pose.py
+++ YOLOv8/yolo-pose.py
@@ -1,10 +1,29 @@
+import cv2
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO('yolov8n-pose.pt')
+model = YOLO("yolov8n-pose.pt")  
 
-# Predict with the model
-results = model('2.mp4',show=True) 
+cap = cv2.VideoCapture('2.mp4')
 
-# Extract keypoint
-result_keypoint = results.keypoints.xyn.cpu().numpy()[0]
+
+while True:
+    ret, frame = cap.read()
+    
+    if not ret:
+        break
+    
+    results = model.predict(frame)
+
+    detection = results[0].plot(kpt_line=True, kpt_radius=5, conf=False, boxes=True, labels=False)
+
+    cv2.imshow('YOLOv8 Pose Detection', detection)
+    
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+#result = model(source= '2.mp4',conf=0.5,show=True,save=False)
